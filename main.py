8fcde5252c9585aad2a63bb274750b11465b77f6
Nadesca <43265479+Nadesca@users.noreply.github.com>
2018-09-20 17:56:26 +0200

    Update main.py
    check for winning move (vertical, horizontal, positively sloped diagonal)

--- main.py
+++ main.py
@@ -30,6 +30,52 @@ def drop_piece(board, row, col):
             board[row][col] = turn + 1
 
 
+def check_game_over(board):
+    for col in range(len(board[0])):
+        if board[5][col] == 0:
+            return False
+    print("Game Over!")
+    return True
+
+
+def check_for_winning_move(board, current_row, current_col, turn):
+    # check for vertical win:
+    if current_col > 2:
+        counter = 0
+        for row in range(len(board)):
+            if board[row][current_col] == (turn + 1):
+                counter += 1
+                if counter == 4:
+                    print("Player " + str(turn) + " hat gewonnen!!")
+                    return True
+            else:
+                counter = 0
+    # check_for_horizontal win:
+    counter = 0
+    for col in range(len(board[0])):
+        if board[current_row][col] == (turn + 1):
+            counter += 1
+            if counter == 4:
+                print("Player " + str(turn) + " hat gewonnen!!")
+                return True
+        else:
+            counter = 0
+
+    #check_for_positively sloped_diagonal in relevant places:
+    if current_row < 3 or current_col > 2:
+        counter = 0
+        for col in range(4):
+            for row in range(3):
+                for index in range(0, 4):
+                    if board[row + index][col + index] == (turn + 1):
+                        counter += 1
+                        if counter == 4:
+                            print("Player " + str(turn + 1) + " hat gewonnen!!")
+                            return True
+                else:
+                    counter = 0
+
+
 draw_board()
 game_over = False
 turn = 0
@@ -38,18 +84,28 @@ turn = 0
 while not game_over:
     # Ask for Player 1 Input
     if turn == 0:
-        col = int(input("Player 1 bitte wähle eine Reihe(0-6)"))
-        if is_valid_location(board, col):
-            drop_piece(board, get_next_open_row(board, col), col)
+        current_col = int(input("Player 1 bitte wähle eine Spalte(0-6)"))
+        if is_valid_location(board, current_col):
+            current_row = get_next_open_row(board, current_col)
+            drop_piece(board, current_row, current_col)
             draw_board()
+        if check_for_winning_move(board, current_row, current_col, turn):
+            game_over = True
+        if check_game_over(board):
+            game_over = True
 
     # Ask for Player 2 Input
     if turn == 1:
-        col = int(input("Player 1 bitte wähle eine Reihe(0-6)"))
-        if is_valid_location(board, col):
-            drop_piece(board, get_next_open_row(board, col), col)
+        current_col = int(input("Player 2 bitte wähle eine Spalte(0-6)"))
+        if is_valid_location(board, current_col):
+            current_row = get_next_open_row(board, current_col)
+            drop_piece(board, current_row, current_col)
             draw_board()
-
+        if check_for_winning_move(board, current_row, current_col, turn):
+            game_over = True
+        if check_game_over(board):
+            game_over = True
     turn += 1
     turn = turn % 2
+
 
